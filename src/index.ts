40e526bd9cc9ed9e278e53a607d8bec9b52bf3d9
shay <43248357+shayypy@users.noreply.github.com>
2025-04-01 16:24:47 -0500

    fix: new gocomics layout

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,35 @@ export interface Env {
   WEBHOOK_TOKEN: string;
 }
 
+interface ComicPagePayload {
+  "@context": "https://schema.org";
+  "@type": "ImageObject" | "ComicSeries";
+  // comicseries
+  isAccessibleForFree?: boolean;
+  genre?: string;
+  inLanguage?: string;
+  publisher?: {
+    "@type": string;
+    name: string;
+    url: string;
+    logo: { "@type": string; url: string };
+    sameAs: string[];
+  };
+
+  name: string;
+  description: string;
+  url: string;
+  author: { "@type": string; name: string };
+  contentUrl: string;
+  creator: {
+    "@type": string;
+    name: string;
+    url: string;
+  };
+  datePublished: string;
+  representativeOfPage: boolean;
+}
+
 const stdTimezoneOffset = (date: Date) => {
   const jan = new Date(date.getFullYear(), 0, 1);
   const jul = new Date(date.getFullYear(), 6, 1);
@@ -19,6 +48,10 @@ const isDstObserved = (date: Date) => {
 };
 
 export default {
+  // favicon.ico requests
+  // async fetch() {
+  //   return new Response(null, { status: 204 });
+  // },
   async scheduled(
     event: ScheduledEvent,
     env: Env,
@@ -55,38 +88,77 @@ export default {
       "url",
       `https://www.gocomics.com/${env.GOCOMICS_SLUG}/${formatted}`,
     );
-    url.searchParams.set("selector", "picture.item-comic-image>img");
-    url.searchParams.set("scrape", "attr");
-    url.searchParams.set("attr", "src");
+    const selector = `div[data-sentry-component="ComicViewer"] script[type="application/ld+json"][data-sentry-component="Schema"]`;
+    url.searchParams.set("selector", selector);
+    url.searchParams.set("scrape", "text");
     const response = await fetch(url);
     if (!response.ok) {
       throw Error(`Bad response from scraper: ${response.status}`);
     }
 
-    const data = (await response.json()) as { result: string };
-    if (!data.result) {
-      throw Error(`No image found on ${env.GOCOMICS_SLUG} page (${formatted})`);
+    const data = (await response.json()) as {
+      result: Record<string, string[]>;
+    };
+    console.log("Result:", data.result);
+    if (!data.result || !data.result[selector]?.length) {
+      throw Error(
+        `No suitable data found on ${env.GOCOMICS_SLUG} page (${formatted})`,
+      );
     }
 
-    await fetch(
+    let strip: ComicPagePayload | undefined;
+    let image: Blob | undefined;
+    for (const raw of data.result[selector]) {
+      let parsed: ComicPagePayload;
+      try {
+        parsed = JSON.parse(raw) as ComicPagePayload;
+      } catch {
+        console.log("Failed to parse as JSON:", raw);
+        continue;
+      }
+      console.log("Parsed:", parsed);
+      if (parsed.representativeOfPage && parsed.contentUrl) {
+        console.log("Found good payload with content URL:", parsed.contentUrl);
+        const response = await fetch(parsed.contentUrl, { method: "GET" });
+        // console.log({ response });
+        if (
+          response.ok &&
+          response.headers.get("Content-Type")?.startsWith("image/")
+        ) {
+          strip = parsed;
+          image = await response.blob();
+          break;
+        }
+      }
+    }
+    if (!strip || !image) {
+      throw Error(
+        `No suitable data found on ${env.GOCOMICS_SLUG} page (${formatted}) after ${data.result[selector].length} data scripts`,
+      );
+    }
+
+    console.log("Creating formdata");
+    const form = new FormData();
+    form.set(
+      "payload_json",
+      JSON.stringify({
+        content: `[${strip.name}](<https://www.gocomics.com/${env.GOCOMICS_SLUG}/${formatted}>)`,
+        attachments: [{ id: 0 }],
+        allowed_mentions: { parse: [] },
+      }),
+    );
+    // They are actually type image/gif, but you can pretend they're PNGs and
+    // remove the GIF badge in the corner.
+    form.set("files[0]", image, `${formatted.replace(/\//g, "-")}.png`);
+    console.log(form);
+
+    const discordResponse = await fetch(
       `https://discord.com/api/v10/webhooks/${env.WEBHOOK_ID}/${env.WEBHOOK_TOKEN}`,
       {
         method: "POST",
-        body: JSON.stringify({
-          embeds: [
-            {
-              title: `<t:${Math.floor(event.scheduledTime / 1000)}:D>`,
-              url: url.searchParams.get("url"),
-              image: { url: data.result },
-              color: 0xfefefe,
-              // footer: { text: env.GOCOMICS_SLUG },
-            },
-          ],
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+        body: form,
       },
     );
+    console.log({ discordResponse });
   },
 };
